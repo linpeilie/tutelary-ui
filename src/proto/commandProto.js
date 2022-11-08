57d35ee8d48eb733b2af5a6ecf75ed1e4e62c183
linpl <linpeilie@qq.com>
2022-11-08 21:02:10 +0800

    增加overview命令

--- src/proto/commandProto.js
+++ src/proto/commandProto.js
@@ -2,8 +2,7 @@ export const COMMAND_PATH_PREF = 'src/proto/command/'
 export const TYPE_ARTHAS = 1
 export const TYPE_TUTELARY = 2
 export const COMMAND_PROTO = [
-  { type: '2', command: '20010', result: 'InstanceBaseInfo', description: 'instance base info' },
-  { type: '2', command: '20011', result: 'HostInfo', description: 'instance host info' },
+  { type: '2', command: '20010', result: 'Overview', description: 'instance overview' },
   { type: '2', command: '21010', result: 'EnhanceAffect', description: 'enhance affect' },
   { type: '2', command: '21011', param: 'TraceParam', result: 'TraceResult', description: 'trace method' },
   { type: '2', command: '21099', result: 'EnhanceCommandComplete', description: 'enhance command complete hint' }
