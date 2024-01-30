13f66bdcc203513b8a8976b98c05cc42147163aa
linpeilie <linpeilie@qq.com>
2024-01-30 17:42:45 +0800

    instance detail page add goBack button

--- src/views/instanceDetail/instanceSider.vue
+++ src/views/instanceDetail/instanceSider.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { Icon } from '@iconify/vue'
+import { useRouter } from 'vue-router'
 import type { InstanceDetailInfo } from '@/api/types/instanceTypes'
 
 const props = defineProps<{
   instanceInfo: InstanceDetailInfo
 }>()
+
+const router = useRouter()
 
 const descriptions = computed(() => {
   if (props.instanceInfo) {
@@ -50,92 +53,104 @@ const showSystemProperties = computed(() => {
 const systemPropertiesShowMore = computed(() => {
   return showSystemProperties.value.length !== props.instanceInfo?.systemProperties?.size
 })
+
+function goBack() {
+  if (props.instanceInfo && props.instanceInfo.appName)
+    router.push({ name: 'InstanceList', params: { appName: props.instanceInfo.appName } })
+  else
+    router.push({ name: 'AppList' })
+}
 </script>
 
 <template>
-  <n-card style="width: 300px;" class="cus-scroll-y">
-    <n-descriptions label-placement="top" :column="1">
-      <n-descriptions-item v-for="item of descriptions" :key="item.value">
-        <template #label>
-          <div font-semibold text-gray-400>
-            {{ item.label }}
-          </div>
-        </template>
-        <n-ellipsis style="max-width: 250px;" font-bold>
-          {{ item.value }}
-        </n-ellipsis>
-      </n-descriptions-item>
-      <n-descriptions-item>
-        <template #label>
-          <div font-semibold text-gray-500>
-            ClassPath
-          </div>
-        </template>
-        <n-ellipsis :line-clamp="3" style="max-width: 250px;" :tooltip="false" font-bold>
-          {{ props.instanceInfo.classPath }}
-        </n-ellipsis>
-      </n-descriptions-item>
-      <n-descriptions-item>
-        <template #label>
-          <n-thing>
-            <template #header>
-              <div font-semibold text-gray-500>
-                InputArguments
-              </div>
-            </template>
-            <template v-if="inputArgumentsShowMore" #header-extra>
-              <n-button text>
-                <template #icon>
-                  <n-icon>
-                    <Icon icon="material-symbols:read-more" />
-                  </n-icon>
-                </template>
-                more
-              </n-button>
-            </template>
-          </n-thing>
-        </template>
-        <n-card embedded size="small" :bordered="false" rounded-10>
-          <n-ellipsis v-for="argument of showInputArguments" :key="argument" style="max-width: 200px;" font-bold>
-            {{ argument }}
+  <div flex flex-col>
+    <n-button style="width: 300px" secondary @click="goBack">
+      返回
+    </n-button>
+    <n-card style="width: 300px;" class="cus-scroll-y" mt-10>
+      <n-descriptions label-placement="top" :column="1">
+        <n-descriptions-item v-for="item of descriptions" :key="item.value">
+          <template #label>
+            <div font-semibold text-gray-400>
+              {{ item.label }}
+            </div>
+          </template>
+          <n-ellipsis style="max-width: 250px;" font-bold>
+            {{ item.value }}
           </n-ellipsis>
-        </n-card>
-      </n-descriptions-item>
-      <n-descriptions-item>
-        <template #label>
-          <n-thing>
-            <template #header>
-              <div font-semibold text-gray-500>
-                SystemProperties
-              </div>
-            </template>
-            <template v-if="systemPropertiesShowMore" #header-extra>
-              <n-button text>
-                <template #icon>
-                  <n-icon>
-                    <Icon icon="material-symbols:read-more" />
-                  </n-icon>
-                </template>
-                more
-              </n-button>
-            </template>
-          </n-thing>
-        </template>
-        <n-card embedded size="small" :bordered="false" rounded-10>
-          <n-descriptions label-placement="left" :column="1">
-            <n-descriptions-item v-for="property of showSystemProperties" :key="property[0]">
-              <template #label>
-                {{ property[0] }}
+        </n-descriptions-item>
+        <n-descriptions-item>
+          <template #label>
+            <div font-semibold text-gray-500>
+              ClassPath
+            </div>
+          </template>
+          <n-ellipsis :line-clamp="3" style="max-width: 250px;" :tooltip="false" font-bold>
+            {{ props.instanceInfo.classPath }}
+          </n-ellipsis>
+        </n-descriptions-item>
+        <n-descriptions-item>
+          <template #label>
+            <n-thing>
+              <template #header>
+                <div font-semibold text-gray-500>
+                  InputArguments
+                </div>
               </template>
-              <n-ellipsis style="max-width: 100px;" font-bold>
-                {{ property[1] }}
-              </n-ellipsis>
-            </n-descriptions-item>
-          </n-descriptions>
-        </n-card>
-      </n-descriptions-item>
-    </n-descriptions>
-  </n-card>
+              <template v-if="inputArgumentsShowMore" #header-extra>
+                <n-button text>
+                  <template #icon>
+                    <n-icon>
+                      <Icon icon="material-symbols:read-more" />
+                    </n-icon>
+                  </template>
+                  more
+                </n-button>
+              </template>
+            </n-thing>
+          </template>
+          <n-card embedded size="small" :bordered="false" rounded-10>
+            <n-ellipsis v-for="argument of showInputArguments" :key="argument" style="max-width: 200px;" font-bold>
+              {{ argument }}
+            </n-ellipsis>
+          </n-card>
+        </n-descriptions-item>
+        <n-descriptions-item>
+          <template #label>
+            <n-thing>
+              <template #header>
+                <div font-semibold text-gray-500>
+                  SystemProperties
+                </div>
+              </template>
+              <template v-if="systemPropertiesShowMore" #header-extra>
+                <n-button text>
+                  <template #icon>
+                    <n-icon>
+                      <Icon icon="material-symbols:read-more" />
+                    </n-icon>
+                  </template>
+                  more
+                </n-button>
+              </template>
+            </n-thing>
+          </template>
+          <n-card embedded size="small" :bordered="false" rounded-10>
+            <n-descriptions label-placement="left" :column="1">
+              <n-descriptions-item v-for="property of showSystemProperties" :key="property[0]">
+                <template #label>
+                  {{ property[0] }}
+                </template>
+                <n-ellipsis style="max-width: 100px;" font-bold>
+                  {{ property[1] }}
+                </n-ellipsis>
+              </n-descriptions-item>
+            </n-descriptions>
+          </n-card>
+        </n-descriptions-item>
+      </n-descriptions>
+    </n-card>
+  </div>
 </template>
 
 <style scoped>
